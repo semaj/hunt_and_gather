00416fa22faa17e9150a772bfcf021cd97f1eae6
James Larisch <jamesfordummies@gmaill.com>
2013-10-05 22:02:38 -0400

    lets_break define

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -26,6 +26,7 @@ class TwilioController < ApplicationController
   # private
 
   def gathering_school(text)
+    lets_break = false
     text.keywords.top(10).each do |word|
       School.all.each do |school|
         down_school_name = school.name.downcase
@@ -39,33 +40,35 @@ class TwilioController < ApplicationController
     end
   end
 
-    def gathering_building(school, text)
-      text.keywords.top(10).each do |word|
-        school.buildings.each do |building|
-          down_building_name = building.name.downcase
-          if down_building_name.match(word.text)
-            gathering_room(building, keywords)
-            lets_break = true
-          end
-          break if lets_break
+  def gathering_building(school, text)
+    lets_break = false
+    text.keywords.top(10).each do |word|
+      school.buildings.each do |building|
+        down_building_name = building.name.downcase
+        if down_building_name.match(word.text)
+          gathering_room(building, keywords)
+          lets_break = true
         end
         break if lets_break
       end
+      break if lets_break
     end
+  end
 
-    def gathering_room(building, text)
-      text.keywords.top(10).each do |word|
-        building.rooms.each do |room|
-          down_room_name = room.name.downcase
-          if down_room_name.match(word.text)
-            @response = "There's food in " + room.name + " in " + building.name
-            lets_break = true
-          end
-          break if lets_break
+  def gathering_room(building, text)
+    lets_break = false
+    text.keywords.top(10).each do |word|
+      building.rooms.each do |room|
+        down_room_name = room.name.downcase
+        if down_room_name.match(word.text)
+          @response = "There's food in " + room.name + " in " + building.name
+          lets_break = true
         end
         break if lets_break
       end
-      @response = "Sorry!!!"
+      break if lets_break
     end
+    @response = "Sorry!!!"
+  end
 
 end
