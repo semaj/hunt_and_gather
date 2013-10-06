96057819418a35e1eaa1c811d6e66d2d62c00e7b
James Larisch <jamesfordummies@gmaill.com>
2013-10-05 23:03:47 -0400

    fix

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -24,6 +24,42 @@ class TwilioController < ApplicationController
     render 'process_sms.xml.erb', :content_type => 'text/xml'
   end
 
-  # private
+  private
+  def gathering_school(text)
+    text.keywords.top(10).each do |word|
+      School.all.each do |school|
+        down_school_name = school.name.downcase
+        if down_school_name.match(word.text)
+          return gathering_building(school, text)
+        end
+      end
+    end
+    return "no school"
+  end
+
+  def gathering_building(school, text)
+    text.keywords.top(10).each do |word|
+      school.buildings.each do |building|
+        down_building_name = building.name.downcase
+        if down_building_name.match(word.text)
+          return gathering_room(building, text)
+        end
+      end
+    end
+    return "no building"
+  end
+
+  def gathering_room(building, text)
+    lets_break = false
+    text.keywords.top(10).each do |word|
+      building.rooms.each do |room|
+        down_room_name = room.name.downcase
+        if down_room_name.match(word.text)
+          return "There's food in " + room.name + " in " + building.name
+        end
+      end
+    end
+    return "no room"
+  end
 
 end
